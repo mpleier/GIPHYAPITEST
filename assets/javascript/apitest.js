bb426114b4b26fac52a5ac51522c15fc98d4b1e1
mpleier <mpleier@gmail.com>
2017-08-05 12:41:32 -0700

    finished

--- assets/javascript/apitest.js
+++ assets/javascript/apitest.js
@@ -1,12 +1,18 @@
 var topics = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"];
-var t = "downsized_still";
-
+var t = "fixed_height_still";
+var data = "error";
+var name = "cat";
 
 $(document).ready(function() {
 
+for (i = 0; i<10; i++){
+  $("#wrap").append('<div id="image"' + i + '" value="' + i + '" class="display"></div>');
+}
+
+
 
     function createButton(name) {
-        var x = '<button id=' + name + 'class="ui-button ui-widget ui-corner-all">' + name + '</button>';
+        var x = '<button id=' + name + ' value=' + name + ' class="ui-button ui-widget ui-corner-all">' + name + '</button>';
         return x;
     }
     for (i = 0; i < topics.length; i++) {
@@ -14,47 +20,59 @@ $(document).ready(function() {
 
     }
 
+    function addButton(name) {
+        var x = '<button id=' + name + ' value=' + name + ' class="ui-button ui-widget ui-corner-all">' + name + '</button>';
+        return x;
+
+    }
+
 
 
     function populate(term) {
-        var xhr = $.get(eval("http://api.giphy.com/v1/gifs/search?q=" + term + "&api_key=c9498eda7a034e3aaf97cbe6471bc21b&limit=10"));
-        xhr.done(function(data) {
-            console.log("success got data", data);
+        var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=" + term + "&api_key=c9498eda7a034e3aaf97cbe6471bc21b&limit=10");
+        xhr.done(function(t) {
+          data = t;
+            console.log("success got data", t);
+            $("[id^=image]").html(function() {
+                var e = $(this).attr("value");
+                return "<img src='" + data.data[e].images.fixed_height_still.url + "'><br>Rating: ''" + data.data[e].rating;
+            });
         });
 
 
-
-        $("[id^=image]").html(function() {
-            var e = eval($(this).attr("value"));
-            return "<img src='" + data.data[e].images.downsized_still.url + "'>";
-        });
     }
 
 
 
 
-    $(name).on("click", function() {
 
-    });
 
-    $("[id^=image]").on("click", function() {
+    $(".display").on("click", function() {
         toggle(t);
         var e = eval($(this).attr("value"));
-        $(this).html("<img src=" + eval("data.data[" + e + "].images." + t + ".url") + ">");
+        $(this).html("<img src=" + eval("data.data[" + e + "].images." + t + ".url") + "><br>Rating: " + data.data[e].rating);
     });
 
 
     function toggle() {
-        if (t === "downsized_still") {
-            t = "downsized";
+        if (t === "fixed_height_still") {
+            t = "fixed_height";
         } else {
-            t = "downsized_still";
+            t = "fixed_height_still";
         }
     }
 
 
+    $("#addimal").submit(function( event ) {
+
+      event.preventDefault();
+      $("#topics").append(addButton($( "input:first" ).val()));
+
+    });
 
 
-
+    $("#topics").on("click", ".ui-button", function() {
+populate($(this).attr("value"));
+    });
 
 });
